19e0dbc717b75e41e6f66ba0c16050ffc734ad8b
Sandhya-Sridhar <sandhyaviji2005@gmail.com>
2025-11-10 20:10:11 +0530

    updated

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { clerkWebHooks } from '../controllers/UserController.js'
+import { clerkWebHooks } from '../controllers/UserController'
 
 const userRouter=express.Router()
 
