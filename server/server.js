e46d7afd69299ad27a02abdcc865002194ac0539
Sandhya-Sridhar <sandhyaviji2005@gmail.com>
2025-11-10 19:57:59 +0530

    Added Clerk webhook route and controller setup

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
+import userRouter from './routes/userRoutes.js'
 
 const PORT=process.env.PORT || 4000
 const app=express()
@@ -13,5 +14,5 @@ app.use(cors())
 
 //routes
 app.get("/",(req,res)=>res.send("API working"))
-
+app.use('/api/user',userRouter)
 app.listen(PORT,()=>console.log("Server running"))
